11ad93e57fff915e544726e1376afddb36504496
jaehyungz <blue259@hanmail.net>
2021-07-25 15:00:41 +0900

    firstCommit

---
+++ src/Render.svelte
@@ -0,0 +1,30 @@
+<script>
+	let name = 'kid'
+	let age = 26
+
+function assign() {
+		 name = 'jaehyungz';
+		 age = 27;
+}
+	
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<h2 class={age > 25 ? "active" : ''}>
+		{age}
+	</h2>
+	<img src='' alt={name}/>
+	<input type='text' bind:value={name}/>
+	<button on:click={assign}>assign</button>
+	
+</main>
+
+<style>
+	h1{
+		color:red
+	}
+	.active{
+	color:blue
+	}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import App from "./Render.svelte";
+
+const app = new App({
+  target: document.body,
+  //   props: {
+  //     name: "world",
+  //     age: 25,
+  //   },
+});
+
+export default app;
