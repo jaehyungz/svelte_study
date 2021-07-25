2c6a23041a3d9bd6cb8f8e63eb383cad20627b3c
jaehyungz <blue259@hanmail.net>
2021-07-25 15:39:25 +0900

    Input.svelte 파일 추가

---
+++ src/Input.svelte
@@ -0,0 +1,11 @@
+<script>
+	let text= ''
+</script>
+
+<h1>
+	{text}
+</h1>
+<input type='text' value={text} on:input={(e) => text = e.target.value}/>
+<button on:click={() => text ='SVELTE'}>
+	Click
+</button>
